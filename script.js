756c2468134a8aa1fba0dd1555e5fe96e03a38ab
Diego Camere <xt7753@rimac.com.pe>
2024-04-21 19:37:24 -0500

    Se añade código base y reset css

---
+++ script.js
@@ -0,0 +1,19 @@
+const cache = () => {
+    
+}
+
+const setup = () => {
+    
+}
+
+const bind = () => {
+    
+}
+
+const main = () => {
+    cache();
+    setup();
+    bind();
+}
+
+window.addEventListener('DOMContentLoaded', main)
